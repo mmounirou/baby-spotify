33e93606d96185492b2d4468715232f4eea7e014
mmounirou <mmounirou@mmounirou.com>
2012-11-15 01:59:37 +0100

    initial version of baby-spotify

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,30 +1,168 @@
-// Author: Thomas Davis <thomasalwyndavis@gmail.com>
-// Filename: main.js
+$(function () {
+    var TrackView = Backbone.View.extend({
+        template: _.template($('#track-template').html()),
+        render: function() {
+              this.$el.html(this.template(this.model.toJSON()));
+              return this;
+        }
+    });
 
-// Require.js allows us to configure shortcut alias
-// Their usage will become more apparent futher along in the tutorial.
-require.config({
-  paths: {
-    jquery: 'libs/jquery/jquery-min',
-    underscore: 'libs/underscore/underscore-min',
-    backbone: 'libs/backbone/backbone-optamd3-min',
-    bootstrap: 'libs/bootstrap/bootstrap-min',
-    text: 'libs/require/text',
-    templates: '../templates'
-  },
-  urlArgs: "bust=" +  (new Date()).getTime()  //TODO: cache-buster: remove for production!!!
+    var AlbumView = Backbone.View.extend({
+        template: _.template($('#album-template').html()),
+        render: function() {
+              this.$el.html(this.template(this.model.toJSON()));
+              return this;
+        }
+    });
+    
+    var ArtistView = Backbone.View.extend({
+        template: _.template($('#artist-template').html()),
+        render: function() {
+              this.$el.html(this.template(this.model.toJSON()));
+              return this;
+        }
+    });
+    
+    var TracksView = Backbone.View.extend({
+        initialize : function(){
+            this.collection.each(this.add);
+            _(this).bindAll('add', 'remove');
+        },
+        add : function(track){
+            var dv = new TrackView({
+                tagName : 'li',
+                model : track
+            });
+            $(this.el).append(dv.render().el);
+        },
+        el:$("#tracks-result"),
+        render : function() {
+            $(this.el).empty();
+            this.collection.each(this.add);
+            return this;
+          }
+    });
+    
+    var ArtistsView = Backbone.View.extend({
+        initialize : function(){
+            this.collection.each(this.add);
+            _(this).bindAll('add', 'remove');
+        },
+        add : function(artist){
+            var dv = new ArtistView({
+                tagName : 'li',
+                model : artist
+            });
+            $(this.el).append(dv.render().el);
+        },
+        el:$("#artists-result"),
+        render : function() {
+            $(this.el).empty();
+            this.collection.each(this.add);
+            return this;
+          }
+    });
+    
+    var AlbumsView = Backbone.View.extend({
+        initialize : function(){
+            this.collection.each(this.add);
+            _(this).bindAll('add', 'remove');
+        },
+        add : function(album){
+            var dv = new AlbumView({
+                tagName : 'li',
+                model : album
+            });
+            $(this.el).append(dv.render().el);
+        },
+        el:$("#albums-result"),
+        render : function() {
+            $(this.el).empty();
+            this.collection.each(this.add);
+            return this;
+          }
+    });
+    
+    var Tracks = Backbone.Collection.extend({
+        parse: function(response) {
+            return response.tracks;
+        }
+    });
+    
+    var Artists = Backbone.Collection.extend({
+        parse: function(response) {
+            return response.artists;
+        }
+    });
+    
+    var Albums = Backbone.Collection.extend({
+        parse: function(response) {
+            return response.albums;
+        }
+    });
+    
+    var AppView = Backbone.View.extend({
+        el: $("#spotifyapp"),
+        
+        events:{
+            "keypress #search": "search" 
+        },
+        search : function(e){
+          if (e.keyCode != 13) return;
+          if (!this.input.val()) return;
 
-});
+        var that = this;
+        this.tracksResults.url = "http://ws.spotify.com/search/1/track.json?q="+this.input.val();
+        this.tracksResults.fetch({ success: function(){that.tracksView.render();}});
+            
+        this.artistsResults.url = "http://ws.spotify.com/search/1/artist.json?q="+this.input.val();
+        this.artistsResults.fetch({ success: function(){that.artistsView.render();}});
+            
+        this.albumsResults.url = "http://ws.spotify.com/search/1/album.json?q="+this.input.val();
+        this.albumsResults.fetch({ success: function(){that.albumsView.render();}});
+        
+        },
+        initialize: function() {
+            console.log("test initialize");
+            this.input = this.$("#search");
+            this.tracksResults = new Tracks();
+            this.albumsResults = new Albums();
+            this.artistsResults = new Artists();
 
-require([
-
-  // Load our app module and pass it to our definition function
-  'app'
-
-  // Some plugins have to be loaded in order due to their non AMD compliance
-  // Because these scripts are not "modules" they do not pass any values to the definition function below
-], function(App){
-  // The "app" dependency is passed in as "App"
-  // Again, the other dependencies passed in are not "AMD" therefore don't pass a parameter to this function
-  App.initialize();
+            this.tracksView = new TracksView({collection : this.tracksResults});
+            this.albumsView = new AlbumsView({collection : this.albumsResults});
+            this.artistsView = new ArtistsView({collection : this.artistsResults});
+ 
+        }
+        ,clean : function(){
+            this.tracksResults.reset();
+            this.albumsResults.reset();
+            this.artistsResults.reset();
+        }
+    }); 
+    
+    var AppRouter = Backbone.Router.extend({
+          routes: {
+            "track/:query":        "track",
+            "album/:query":        "album", 
+            "artist/:query":       "artist",
+          },
+        
+          artist: function(query){
+              var view = mainAppView;
+              view.clean();
+              var artist = new Backbone.Model({id:query});
+              artist.url="http://ws.spotify.com/lookup/1/.json?uri="+query+"&extras=album"
+              artist.fetch({ success: function(){
+                  view.albumsResults.add(artist.get("artist").albums.map(function(album){
+                      return album.album;
+                  }));
+                  view.albumsView.render();
+              }}); 
+          }
+    });
+    
+    var mainAppView = new AppView;
+    var mainRouter = new AppRouter;
+    Backbone.history.start()
 });
